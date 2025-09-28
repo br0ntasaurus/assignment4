dbf9fe6a15777353b84a2884791cbfcb79bea37c
br0ntasaurus <158488726+br0ntasaurus@users.noreply.github.com>
2025-09-28 13:56:42 -0400

    completed time challenge problem

--- timed_challenge.py
+++ timed_challenge.py
@@ -1,3 +1,19 @@
 # Pick one question from timed_challenge.txt
 # Paste the question as a comment below
 # Set a timer for 30 minutes and complete the question!
+
+
+
+# First Repeated Value
+# Return the first value that repeats in the collection.
+# Input: [1, 4, 3, 5, 3, 2, 1]
+# Output: 3
+
+
+def first_repeated_value(values):
+    seen = set ()
+    for item in values:
+        if item in seen:
+            return item
+        seen.add(item)
+    return None
